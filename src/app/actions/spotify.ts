e9b786f2f3cc5eb3fa2dc84539972c756775a754
Spandan-Mishra <spandanmishra69@gmail.com>
2025-12-11 15:22:04 +0530

    update: top tracks for spotify

--- src/app/actions/spotify.ts
+++ src/app/actions/spotify.ts
@@ -1,19 +1,24 @@
 "use server";
 
-import { getTopArtists } from "@/lib/spotify";
+import { getTopArtists, getTopTracks } from "@/lib/spotify";
 import { Tone } from "../page";
 import generateResponse from "@/lib/llm";
 
 export default async function analyzeSpotify({ accessToken, tone }:{accessToken: string, tone: Tone}) {
-    const data = await getTopArtists({ accessToken });
+    const artistData = await getTopArtists({ accessToken });
+    const trackData = await getTopTracks({ accessToken });
 
-    if (!data || !data.items) {
+    if (!artistData || !artistData.items) {
         throw new Error("Failed to fetch Spotify data");
     }
 
-    const artistNames = data.items.map((artist: any) => artist.name).join(", ");
-    
-    const feedback = await generateResponse(`list of top artists: ${artistNames}`, tone);
+    const artistNames = artistData.items.map((artist: any) => artist.name).join(", ");
+    const trackNames = trackData.items.map((track: any) => track.name).join(", ");
+
+    console.log("Artist Names:", artistNames);
+    console.log("Track Names:", trackNames);
+
+    const feedback = await generateResponse(`list of top artists: ${artistNames}, list of top tracks: ${trackNames}`, tone);
 
     return feedback;
 }
--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,7 +1,17 @@
 "use server";
 
 export async function getTopArtists({ accessToken }: {accessToken: string}) {
-    const res = await fetch("https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=15", {
+    const res = await fetch("https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=5", {
+        headers: {
+            Authorization: "Bearer " + accessToken
+        }
+    })
+
+    return res.json();
+}
+
+export async function getTopTracks({ accessToken }: {accessToken: string}) {
+    const res = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=20", {
         headers: {
             Authorization: "Bearer " + accessToken
         }
